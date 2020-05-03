4acb105dfa801be3536527f239590f27a7714e4f
Meet Parikh <meet.parikh@paconsulting.com>
2020-05-03 16:54:30 +0100

    Scaffolding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'IsstoChamval';
+export class AppComponent implements OnInit {
+  kaudis: number;
+  clickTime: Date;
+  private NORMAL_NUMBERS = [1, 2, 3];
+  private FOUR_NUMBERS = 4;
+  private EIGHT_NUMBERS = 8;
+  private colours = ['AntiqueWhite', 'White', 'AliceBlue', 'Aqua', 'Azure', 'Ivory', 'HoneyDew', 'Lavender', 'LavenderBlush', 'Khaki',
+    'LemonChiffon', 'LightGreen', 'LightSalmon', 'LightSkyBlue', 'LightSeaGreen'
+  ];
+  bgColour: string;
+
+  ngOnInit(): void {
+    this.nextKaudis();
+  }
+
+  nextKaudis() {
+    const probability = Math.random();
+    if (probability > 0.92) {
+      this.kaudis = this.EIGHT_NUMBERS;
+    } else if (probability > 0.75) {
+      this.kaudis = this.FOUR_NUMBERS;
+    } else {
+      this.kaudis = _.sample(this.NORMAL_NUMBERS);
+    }
+    this.clickTime = new Date();
+    this.bgColour = _.sample(this.colours);
+  }
 }
